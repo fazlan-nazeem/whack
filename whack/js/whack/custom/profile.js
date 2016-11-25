551b9063865e31347a4a9abc0a7f41141a8f4a31
thusithakalugamage <thusitha32@yahoo.com>
2016-11-26 03:13:05 +0530

    profile page UX - crawler styling fixes

--- whack/js/whack/custom/profile.js
+++ whack/js/whack/custom/profile.js
@@ -46,10 +46,49 @@ $( document ).ready(function() {
 
 });
 
-
-
 var viewData = function (data) {
     $("#account-profile").html(data);
     $(".references").remove();
     $(".ambox-Cleanup").remove();
 };
+
+
+
+equalheight = function(container){
+var currentTallest = 0,
+     currentRowStart = 0,
+     rowDivs = new Array(),
+     $el,
+     topPosition = 0;
+ $(container).each(function() {
+
+   $el = $(this);
+   $($el).height('auto')
+   topPostion = $el.position().top;
+
+   if (currentRowStart != topPostion) {
+     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+       rowDivs[currentDiv].height(currentTallest);
+     }
+     rowDivs.length = 0; // empty the array
+     currentRowStart = topPostion;
+     currentTallest = $el.height();
+     rowDivs.push($el);
+   } else {
+     rowDivs.push($el);
+     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+  }
+   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+     rowDivs[currentDiv].height(currentTallest);
+   }
+ });
+}
+
+$(window).load(function() {
+  equalheight('.panel-same-height');
+});
+
+
+$(window).resize(function(){
+  equalheight('.panel-same-height');
+});
