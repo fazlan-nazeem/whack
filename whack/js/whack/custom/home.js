1d58255686ca571bcad071b12ffafed93c800769
thusithakalugamage <thusitha32@yahoo.com>
2016-11-25 20:52:54 +0530

    home page UX

--- whack/js/whack/custom/home.js
+++ whack/js/whack/custom/home.js
@@ -13,6 +13,54 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-(function () {
 
-})();
+ $( document ).ready(function() {
+
+    //viewerRoles.initialize();
+
+
+    var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+      'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+      'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+      'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+      'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+      'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+      'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+      'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+      'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+    ];
+
+    var typeaheadElem = $('#scrollable-dropdown-menu .typeahead').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 1
+    }, {
+        name: 'states',
+        limit: 10,
+        source: substringMatcher(states)
+    }).on('typeahead:selected', function (e, role, roles) {
+        //addViewers($(this), role.name);
+    }).on('typeahead:autocomplete', function (e, role) {
+        //addViewers($(this), role.name);
+    });
+    $(typeaheadElem).width($(typeaheadElem).parent().parent().width() - 30);
+ });
+
+var substringMatcher = function(strs) {
+      return function findMatches(q, cb) {
+        var matches, substringRegex;
+
+        matches = [];
+
+        substrRegex = new RegExp(q, 'i');
+
+        $.each(strs, function(i, str) {
+          if (substrRegex.test(str)) {
+            matches.push(str);
+          }
+        });
+
+        cb(matches);
+      };
+    };
+
